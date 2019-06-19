5b0f8649cd19325cd9ab146715a836be6401e7f2
Dominic Henry <dominichenrywork@hotmail.com>
2019-06-19 11:04:05 -0500

    Completion of Initial script, V1.0

--- jseApp.py
+++ jseApp.py
@@ -8,7 +8,7 @@
 import os
 import sys
 from bs4 import BeautifulSoup
-from csv import DictReader
+import csv
 from urllib import request
 import lxml
 import datetime
@@ -29,7 +29,7 @@ def retrieve_csv_files(st_date, end_date):
         print(dateStr)
 
         # Makes request to JSE three times for a page before throwing exception
-        for i in range(3):
+        for i in range(4):
             try:
                 # Making request to JSE market-quote page to junior market index 
                 jse_request = request.urlopen('https://www.jamstockex.com/market-data/junior-market/quote/' + dateStr) 
@@ -52,16 +52,44 @@ def retrieve_csv_files(st_date, end_date):
             except:
                 continue
 
+# Pulls the stock name and closed price for each day and retuns as a dictionary
+def pullStockData(st_date, end_date):
+    stock_list = dict()
+
+    dateStartObj = datetime.datetime.strptime(st_date, "%Y-%m-%d")
+    dateEndObj = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+
+    while dateStartObj <= dateEndObj:
+        dateStr = dateStartObj.strftime("%Y-%m-%d")
+
+        with open('temp/' + dateStr + '.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if(row != []):
+                    if(row[0] not in ['Security', 'Preference Shares', 'Index Data', 'Index', 'JSE-Junior', 'Ordinary Shares']):
+                        if(row[0] not in stock_list):
+                            stock_list[row[0]] = [float(row[2])] 
+                        else:
+                            stock_list[row[0]].append(float(row[2]))
+
+        dateStartObj += datetime.timedelta(days=1)
+
+    return stock_list 
+
+# Takes a dictionary containing stock name and close prices 
+# Returns a list of tuples with (Stock Tag, Net Chnage)
+def processData(stock_list):
+    return [(keys, float("{0:.2f}".format(sum(values)/len(values)))) for keys, values in stock_list.items()]
+
 
 # Main
 if __name__ == "__main__":
-    # Creates an empty dictionary to hold all stock data 
-    stock_list = dict() 
-
     # Retrieves csv files from JSE website
-    retrieve_csv_files("2019-01-31", "2019-06-30")
+    retrieve_csv_files(sys.argv[1], sys.argv[2])
 
     # Pulls Stock name and Close Prices from csv files and places into stock_list
+    stock_list = pullStockData(sys.argv[1], sys.argv[2])
+    stock_info_list = processData(stock_list)
 
-
-
+    for i in stock_info_list:
+        print(i)
